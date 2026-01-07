e36ca29e4ed1579a19994ae63b693c1c91e52fe8
tommasino03 <tommytommyrm@gmail.com>
2026-01-07 21:32:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import os
-from telegram import Bot
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def first_message(update, context):
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(
+        f"✅ CHAT_ID CORRETTO TROVATO:\n{chat_id}"
+    )
 
 def main():
-    token = os.getenv("TELEGRAM_TOKEN")
-    chat_id = os.getenv("CHAT_ID")
-
-    bot = Bot(token=token)
-
-    bot.send_message(
-        chat_id=chat_id,
-        text="✅ TEST OK: se leggi questo messaggio, il bot può scrivere"
-    )
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, first_message))
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
+
